ee2d8143d946986b2d6f4f00986196889b1abaa1
Vjacheslav Vortex <slouffka@gmail.com>
2012-11-29 12:15:45 +0700

    Refactoring server and client. Not working commit!

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = require('express')()
 
 var users = [];
 
+// start the server
 server.listen(80);
 
 // serving main page
@@ -13,30 +14,64 @@ app.get('/', function (req, res) {
 
 // serving user connection
 io.sockets.on('connection', function (socket) {
-  var username;
+  var user = {
+    socket: socket,
+    connected: 'undefined',
+    name: 'undefined'
+  };
 
-  socket.broadcast.emit('user connected');
-
-  socket.emit('message', { body: 'You are now connected to Chat.js v0.01a' });
+  // welcome message to the user
+  socket.emit('system', {
+    type: 'connected',
+    user: 'Chat.js',
+    body: 'You are now connected to Chat.js v0.01a'
+  });
  
-  // user has set his name 
+  // user has set his name (authencticated)
   socket.on('set name', function (name) {
   	socket.set('name', name, function () {
-      username = name;
-      users.push(username);
-  		socket.emit('status', {status: 'ready', name: name});
+      user.name = name;
+      users.push(user); // add into user list
+
+      // inform user that he is authenticated
+      socket.emit('system', {
+        type: 'name set',
+        user: 'Chat.js',
+        name: name,
+        body: 'You are now known as ' + name
+      });
+
+      // broadcast user join to others
+  		socket.broadcast.emit('system', {
+        type: 'joined',
+        user: 'Chat.js',
+        name: name,
+        body: 'User ' + name + ' has joined the chat'
+      });
   	});
+  });
+
+  // return users list
+  socket.on('get users', function () {
+    socket.emit('users', users);
   });
 
   // user sent a message
   socket.on('message', function (data) {
   	socket.get('name', function (err, name) {
-  		io.sockets.emit('message', {name: name, body: data});
+  		io.sockets.emit('message', {
+        user: name, body: data
+      });
   	});
   });
 
+  // user left the chat
   socket.on('disconnect', function () {
-    users.indexOf(username);
-  	io.sockets.emit('user disconnected');
+  	socket.broadcast.emit('system', {
+      type: 'left',
+      user: 'Chat.js',
+      name: user.name,
+      body: 'User ' + user.name + ' has left the chat.'
+    });
   });
 });
